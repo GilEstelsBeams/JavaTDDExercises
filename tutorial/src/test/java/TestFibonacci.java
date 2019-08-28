5474ad4e2a7f51a95aa6800cb268b58ae2c61a5f
Rose <47150066+GilEstelsBeams@users.noreply.github.com>
2019-08-28 15:08:19 +0100

    added testing suite

---
+++ tutorial/src/test/java/TestFibonacci.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestFibonacci {
+
+
+    @Test
+    public void testIsFibonacci() {
+        boolean isFibonacci = true;
+        assertEquals(isFibonacci,true);
+    }
+}
---
+++ tutorial/src/test/java/TestPrimeNumbers.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestPrimeNumbers {
+
+
+    @Test
+    public void testIsPrimeNumber() {
+        boolean isPrimeNumber = true;
+        assertEquals(isPrimeNumber,true);
+    }
+}
