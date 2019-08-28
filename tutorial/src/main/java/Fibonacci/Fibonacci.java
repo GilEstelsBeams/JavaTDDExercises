6a564656ff743f57ea2daedb325d7f85271d9c5f
Rose <47150066+GilEstelsBeams@users.noreply.github.com>
2019-08-28 15:33:02 +0100

    Fibonacci sequence working

--- tutorial/src/main/java/Fibonacci/Fibonacci.java
+++ tutorial/src/main/java/Fibonacci/Fibonacci.java
@@ -1,16 +1,29 @@
 package Fibonacci;
 
 public class Fibonacci {
+
+    public Fibonacci() {
+    }
+
     public int fib(int n){
+        int result=0;
+
+        if(n<0) {
+            System.out.println("You entered a negative number");
+            result = -1;
+        }
+        else{
 
         if(n==0 || n==1) return n;
-        int twoBefore=0;
-        int oneBefore=1;
-        int result=0;
-        for(int i=2;i<=n; i++){
-            result = twoBefore + oneBefore;
-            twoBefore=oneBefore;
-            oneBefore=result;
+            else{
+            int twoBefore=0;
+            int oneBefore=1;
+
+            for(int i=2;i<=n; i++) {
+                result = twoBefore + oneBefore;
+                twoBefore = oneBefore;
+                oneBefore = result; }
+               }
         }
         return result;
     }
--- tutorial/src/test/java/TestFibonacci.java
+++ tutorial/src/test/java/TestFibonacci.java
@@ -1,12 +1,43 @@
+import Fibonacci.Fibonacci;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
 
 public class TestFibonacci {
 
 
     @Test
-    public void testIsFibonacci() {
-        boolean isFibonacci = true;
-        assertEquals(isFibonacci,true);
+    public void testMakesSimpleFibonacci() {
+        Fibonacci fibonacci=new Fibonacci();
+        int number= fibonacci.fib(3);
+        assertEquals(2,number);
+    }
+
+    @Test
+    public void testZeroFibonacci() {
+        Fibonacci fibonacci=new Fibonacci();
+        int number= fibonacci.fib(0);
+        assertEquals(0,number);
+    }
+
+    @Test
+    public void testOneFibonacci() {
+        Fibonacci fibonacci=new Fibonacci();
+        int number= fibonacci.fib(1);
+        assertEquals(1,number);
+    }
+
+    @Test
+    public void testNegativeFibonacci() {
+        Fibonacci fibonacci=new Fibonacci();
+        int number= fibonacci.fib(-5);
+        assertEquals(-1,number);
+    }
+
+    @Test
+    public void testLargeFibonacci() {
+        Fibonacci fibonacci=new Fibonacci();
+        int number= fibonacci.fib(17);
+        assertEquals(1597,number);
     }
 }
