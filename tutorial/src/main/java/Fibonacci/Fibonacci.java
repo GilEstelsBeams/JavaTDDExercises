b2d43a9a6aeb206dfc630345d37fac115f595dfb
Rose <47150066+GilEstelsBeams@users.noreply.github.com>
2019-08-28 15:18:28 +0100

    computing Fibonacci sequence

--- tutorial/src/main/java/Fibonacci/Fibonacci.java
+++ tutorial/src/main/java/Fibonacci/Fibonacci.java
@@ -2,9 +2,15 @@ package Fibonacci;
 
 public class Fibonacci {
     public int fib(int n){
-        int result=1;
-        for(int i=0;i<n; i++){
-            result += result;
+
+        if(n==0 || n==1) return n;
+        int twoBefore=0;
+        int oneBefore=1;
+        int result=0;
+        for(int i=2;i<=n; i++){
+            result = twoBefore + oneBefore;
+            twoBefore=oneBefore;
+            oneBefore=result;
         }
         return result;
     }
