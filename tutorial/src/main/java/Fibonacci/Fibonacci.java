cfbae6d69e12dc764c2c39a687331b6dc307ee61
Rose <47150066+GilEstelsBeams@users.noreply.github.com>
2019-08-28 15:40:45 +0100

    changed int to long

--- tutorial/src/main/java/Fibonacci/Fibonacci.java
+++ tutorial/src/main/java/Fibonacci/Fibonacci.java
@@ -5,8 +5,8 @@ public class Fibonacci {
     public Fibonacci() {
     }
 
-    public int fib(int n){
-        int result=0;
+    public long fib(int n){
+        long result=0;
 
         if(n<0) {
             System.out.println("You entered a negative number");
@@ -16,8 +16,8 @@ public class Fibonacci {
 
         if(n==0 || n==1) return n;
             else{
-            int twoBefore=0;
-            int oneBefore=1;
+            long twoBefore=0;
+            long oneBefore=1;
 
             for(int i=2;i<=n; i++) {
                 result = twoBefore + oneBefore;
--- tutorial/src/test/java/TestFibonacci.java
+++ tutorial/src/test/java/TestFibonacci.java
@@ -9,35 +9,42 @@ public class TestFibonacci {
     @Test
     public void testMakesSimpleFibonacci() {
         Fibonacci fibonacci=new Fibonacci();
-        int number= fibonacci.fib(3);
+        long number= fibonacci.fib(3);
         assertEquals(2,number);
     }
 
     @Test
     public void testZeroFibonacci() {
         Fibonacci fibonacci=new Fibonacci();
-        int number= fibonacci.fib(0);
+        long number= fibonacci.fib(0);
         assertEquals(0,number);
     }
 
     @Test
     public void testOneFibonacci() {
         Fibonacci fibonacci=new Fibonacci();
-        int number= fibonacci.fib(1);
+        long number= fibonacci.fib(1);
         assertEquals(1,number);
     }
 
     @Test
     public void testNegativeFibonacci() {
         Fibonacci fibonacci=new Fibonacci();
-        int number= fibonacci.fib(-5);
+        long number= fibonacci.fib(-5);
         assertEquals(-1,number);
     }
 
     @Test
     public void testLargeFibonacci() {
         Fibonacci fibonacci=new Fibonacci();
-        int number= fibonacci.fib(17);
+        long number= fibonacci.fib(17);
         assertEquals(1597,number);
+    }
+
+    @Test
+    public void testFiftiethFibonacci() {
+        Fibonacci fibonacci=new Fibonacci();
+        long number= fibonacci.fib(50);
+        assertEquals(12586269025L,number);
     }
 }
