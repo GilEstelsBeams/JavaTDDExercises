dddbf2e990e7343694b447d157e447dac998d38b
Rose <47150066+GilEstelsBeams@users.noreply.github.com>
2019-09-04 16:43:03 +0100

    changed original method to use second method

--- Exercises/src/main/java/PrimeNumbers/PrimeNumber.java
+++ Exercises/src/main/java/PrimeNumbers/PrimeNumber.java
@@ -5,36 +5,44 @@ import java.util.List;
 
 public class PrimeNumber {
 
+    // CHECKS IF INTEGER IS PRIME NUMBER
+
     public boolean isPrime(int n) {
-        if (n < 2) return false;
-        else {
-            if (n == 2) return true;
-
-            else {
-                ArrayList<Integer> primes = new ArrayList<Integer>();
-                primes.add(2);
-
-
-                for (int i = 3; i <= n; i++) {
-
-                    boolean maybePrime =true;
-
-                        for (int j = 0; j < primes.size(); j++) {
-                            int remainder = i % primes.get(j);
-                            if (remainder == 0){maybePrime=false;
-                            break;}
-                        }
-
-                    if(maybePrime)primes.add(i);
-                    }
-                    return primes.contains(n);
-                }
-            }
+        return primes2(n).contains(n);
         }
+
+        // METHOD TO EXPERIMENT WITH ATHREADPOOLEXECUTOR EVEN THOUGH MIGHT BE LESS EFFICIENT THAN SECOND ONE
 
     public List<Integer> primes(int n){
         List<Integer> listPrimes= new ArrayList<Integer>();
         return listPrimes;
+    }
+
+        //METHOD USED IN ISPRIME TO DETERMINE LIST
+
+    public List<Integer> primes2(int n){
+        List<Integer> listPrimes= new ArrayList<Integer>();
+        if (n < 2) return listPrimes;
+        else {
+            listPrimes.add(2);
+            if (n == 2) return listPrimes;
+
+            else {
+                for (int i = 3; i <= n; i++) {
+
+                    boolean maybePrime =true;
+
+                    for (int j = 0; j < listPrimes.size(); j++) {
+                        int remainder = i % listPrimes.get(j);
+                        if (remainder == 0){maybePrime=false;
+                            break;}
+                    }
+
+                    if(maybePrime)listPrimes.add(i);
+                }
+                return listPrimes;
+            }
+        }
     }
     }
 
