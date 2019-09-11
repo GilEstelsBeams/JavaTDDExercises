619df6f14cb6201917660478632ee894662cfe29
Rose <47150066+GilEstelsBeams@users.noreply.github.com>
2019-09-11 16:04:58 +0100

    Added the four list of same size

--- Exercises/src/main/java/PrimeNumbers/PrimeNumber.java
+++ Exercises/src/main/java/PrimeNumbers/PrimeNumber.java
@@ -11,7 +11,8 @@ public class PrimeNumber {
     public static void main(String[] args) {
         PrimeNumber pN=new PrimeNumber();
         System.out.println(pN.isPrime(112333));
-        System.out.println(pN.primes2(112314));
+        System.out.println(pN.primes2(100));
+        System.out.println(pN.primes(100));
     }
 
     // CHECKS IF INTEGER IS PRIME NUMBER
@@ -25,24 +26,64 @@ public class PrimeNumber {
     public List<Integer> primes(int n) {
         List<Integer> listPrimes = new ArrayList<Integer>();
         if (n > 0) {
+            if(n>3) {
                 ExecutorService executor = Executors.newFixedThreadPool(4);
+                 int parts = n / 4;
+                Future<List<Integer>> answer1 = executor.submit(() -> {
+                    return listBoundedPrimes(1, parts);
+                });
+                Future<List<Integer>> answer2 = executor.submit(() -> {
+                    return listBoundedPrimes(parts + 1, 2 * parts);
+                });
+                Future<List<Integer>> answer3 = executor.submit(() -> {
+                    return listBoundedPrimes(2 * parts + 1, 3 * parts);
+                });
+                Future<List<Integer>> answer4 = executor.submit(() -> {
+                    return listBoundedPrimes(3 * parts + 1, n);
+                });
+                    try {
+                            listPrimes.addAll(answer1.get());
+                            listPrimes.addAll(answer2.get());
+                            listPrimes.addAll(answer3.get());
+                            listPrimes.addAll(answer4.get());
 
-                for(int i=1; i<=n;i++){
-                    final int j=i;
-                    Future<Boolean> answer= executor.submit (()->{
-                        return isPrime(j);
-                    });
+                    } catch (Exception e) {
+                    }
 
-                   try{ if(answer.get()){ listPrimes.add(i);}
-                   }
-                   catch(Exception e){
-                       }
-                }
                 executor.shutdown();
-           }
+            }
+            listPrimes=listBoundedPrimes(1,n);
+          }
+
+        // SIMPLE CODE WITHOUT THE FOUR LISTS
+
+//           for(int i=1; i<=n;i++){
+//                    final int j=i;
+//                    Future<Boolean> answer= executor.submit (()->{
+//                        return isPrime(j);
+//                    });
+//
+//                   try{ if(answer.get()){ listPrimes.add(i);}
+//                   }
+//                   catch(Exception e){
+//                       }
+//                }
+//                executor.shutdown();
+//           }
 
         return listPrimes;
     }
+
+    //METHOD USED IN EXECUTOR
+
+public List<Integer> listBoundedPrimes(int start, int finish){
+       List<Integer> listPrimes = new ArrayList<Integer>();
+        for(int i=start; i<=finish; i++){
+            if(isPrime(i)) listPrimes.add(i);
+        }
+        return listPrimes;
+}
+
 
         //METHOD USED IN ISPRIME TO DETERMINE LIST
 
