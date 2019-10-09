f1ff8167680033427d51f9c3c6cbcf0f11710041
Rose <47150066+GilEstelsBeams@users.noreply.github.com>
2019-10-09 16:09:05 +0100

    added check function

--- Exercises/src/main/java/WeatherForecast/WeatherForecast.java
+++ Exercises/src/main/java/WeatherForecast/WeatherForecast.java
@@ -1,4 +1,21 @@
 package WeatherForecast;
 
+import java.util.Arrays;
+
 public class WeatherForecast {
+private double rainRain;
+private double rainSun;
+private double sunSun;
+private double sunRain;
+
+public WeatherForecast(){
+
+}
+
+
+private boolean checkProbabilities(){
+    double[] checks= new double[] {rainSun+rainRain, rainRain+sunRain, sunRain+sunSun, rainSun+sunSun};
+    double[] checksTrue= new double[] {1.0,1.0,1.0,1.0};
+    return Arrays.equals(checks,checksTrue);
+}
 }
